3d7bfed44635f2d50ef34b7d6e403fab4867fd73
Taiel <canada.taiel@gmail.com>
2024-02-14 19:30:20 -0300

    sape

---
+++ primerVite/primerVite/src/app.jsx
@@ -0,0 +1,45 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Sumar al contador
+        </button>
+        <button>count is {count}</button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Restar al contador
+        </button>
+      </div>
+      <div class="card">
+        <button onClick={() => setCount((count) => count * 2)}>
+          Multiplicar al contador por 2
+        </button>
+        <button onClick={() => setCount((count) => Math.round(count / 2))}>
+          Dividir al contador por 2
+        </button>
+      </div>
+      <p>
+        Edit <code>src/app.jsx</code> and save to test HMR
+      </p>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  );
+}
